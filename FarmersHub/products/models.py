977507e11e7f1b91ada14e294ec986bde38e7fb0
tAdithyan <adithyanttech@gmil.com>
2024-04-26 14:30:33 +0530

    product model created

--- FarmersHub/products/models.py
+++ FarmersHub/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# model for iteams.
+class product(models.Model):
+  LIVE=1
+  DELETE=0
+  DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+  title=models.CharField(max_length=200)
+  price=models.FloatField()
+  description=models.TextField()
+  image=models.ImageField(upload_to='/media')
+  priority=models.IntegerField(default=0)
+  created_at=models.DateTimeField(auto_now_add=True)
+  updaed_at=models.DateTimeField(auto_now=True)
+
+
+
+  def __str__(self):
+    return self.title 
