46d8eee58b53a805af54336a9d3295f68aa2a1bf
tAdithyan <adithyanttech@gmil.com>
2024-04-26 14:45:38 +0530

    cart model created

--- FarmersHub/orders/models.py
+++ FarmersHub/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from customers.models import Customer
 # Create your models here.
+class Cart(models.Model):
+  LIVE=1
+  DELETE=0
+  DELETE_CHOICES=((LIVE,'Live'),(DELETE, 'Delete'))
+  owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
+  delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+  created_at=models.DateTimeField(auto_now_add=True)
+  updated_at=models.DateTimeField(auto_now=True)
+  
